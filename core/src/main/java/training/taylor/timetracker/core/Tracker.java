5f1734ba292f69d2b8a85df4e5e0e74d9cbe30ce
e10254 <39467184+e10254@users.noreply.github.com>
2018-05-26 17:48:08 +0100

    COMMENT

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
+// COMMENT 
 import java.util.List;
 
 @Component
